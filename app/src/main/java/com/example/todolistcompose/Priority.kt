5e44daa897e1075781ace525b4245c6fd0d71234
Timothée Jouffrieau <timothee.jouffrieau.ext@inetum.com>
2023-10-06 00:08:23 +0200

    Create Task and Priority

---
+++ app/src/main/java/com/example/todolistcompose/Priority.kt
@@ -0,0 +1,7 @@
+package com.example.todolistxml
+
+enum class Priority(val value: Int) {
+    HIGH(0),
+    MEDIUM(1),
+    LOW(2)
+}
---
+++ app/src/main/java/com/example/todolistcompose/Task.kt
@@ -0,0 +1,39 @@
+package com.example.todolistxml
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Task(
+    var id: Int,
+    var title: String= "",
+    var content: String = "",
+    var priority: Priority = Priority.LOW
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString()?.let { Priority.valueOf(it)} ?: Priority.LOW
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(title)
+        parcel.writeString(content)
+        parcel.writeString(priority.name)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Task> {
+        override fun createFromParcel(parcel: Parcel): Task {
+            return Task(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Task?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
